460185b0b7ac3454d0d6975190e7a3ad5dcf0bb0
Danyele Sales <danyelesales@hotmail.com>
2019-01-04 01:21:45 -0200

    alteração de alinhamento css e index

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const button = document.querySelector(".nav__menu-mobile__button")
-const menuMobile = document.querySelector(".nav__mobile");
+const button = document.querySelector(".menu__mobile")
+const menuMobile = document.querySelector(".nav__menu");
 
 
 button.addEventListener( 'click', function(e){
     e.preventDefault();
  
-    if(menuMobile.classList.contains("nav__menu-list__responsive__hidden")){
-        menuMobile.classList.remove("nav__menu-list__responsive__hidden");
-        menuMobile.classList.add(".nav__menu-list__visible")
+    if(menuMobile.classList.contains("nav__menu-list__hidden")){
+        menuMobile.classList.remove("nav__menu-list__hidden");
+        menuMobile.classList.add("nav__menu__visible")
     }else{
-        menuMobile.classList.remove(".nav__menu-list__visible");
-        menuMobile.classList.add("nav__menu-list__responsive__hidden")
+        menuMobile.classList.remove("nav__menu__visible");
+        menuMobile.classList.add("nav__menu-list__hidden")
     }
 
 })
